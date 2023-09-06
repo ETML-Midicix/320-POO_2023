3cfc854fe054f4dd61c285e05b182d07a184761a
ETML-Midicix <pa64wln@eduvaud.ch>
2023-09-06 08:31:13 +0200

    Start ParaClub
    Début Exercice ParaClub

---
+++ exos/ParaClub/ParaClub/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ParaClub
+{
+    internal class Program
+    {
+        static class Config
+        {
+            public const int SCREEN_HEIGHT = 40;
+            public const int SCREEN_WIDTH = 150;
+        }
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+        }
+    }
+}
